51097462cb6d71e20ba21860d5f48fc6a160e4dc
axl-codes <axl.lanca@gmail.com>
2021-01-03 00:49:34 +0100

    adding some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { findReadVarNames } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ps-generator';
+  // Properties
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  password = '';
+
+  //Methods
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+
+
+  // A method that is going to be called anytime a user clicks on that button
+  onButtonClick() {
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@#$%^&*()?";
+
+    let validChars = '';
+
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+    if(this.includeNumbers) {
+      validChars += numbers;
+    }
+    if(this.includeSymbols) {
+      validChars += symbols; 
+    }
+
+    let generatedPassword = "";
+
+    for (let i =0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
